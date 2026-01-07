d5e44dacfd78b29b1bf1437cc875b5d153db293b
joeyscags <joeyscags5000@gmail.com>
2026-01-07 16:07:05 +0100

    Implement main function with Animal and WrongAnimal tests

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,12 +6,43 @@
 /*   By: joeyscags <jcupp@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/07 14:19:11 by joeyscags         #+#    #+#             */
-/*   Updated: 2026/01/07 14:27:12 by joeyscags        ###   ########.fr       */
+/*   Updated: 2026/01/07 15:54:24 by joeyscags        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Animal.hpp"
+#include "Dog.hpp"
+#include "Cat.hpp"
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
 
 int main() {
+    std::cout << "=== Subject Required Tests ===" << std::endl;
+    const Animal* meta = new Animal();
+    const Animal* j = new Dog();
+    const Animal* i = new Cat();
+    
+    std::cout << j->getType() << " " << std::endl;
+    std::cout << i->getType() << " " << std::endl;
+    i->makeSound();
+    j->makeSound();
+    meta->makeSound();
+    
+    delete meta;
+    delete j;
+    delete i;
+    
+    std::cout << "\n=== Wrong Animal Tests ===" << std::endl;
+    const WrongAnimal* wrongMeta = new WrongAnimal();
+    const WrongAnimal* wrongCat = new WrongCat();
+    
+    std::cout << wrongCat->getType() << " " << std::endl;
+    wrongCat->makeSound();
+    wrongMeta->makeSound();
+    
+    delete wrongMeta;
+    delete wrongCat;
+    
     return 0;
 }
+	
